7d1b264437f5e39f9919c8ac9137c11b35aeed7a
mirejy <allo124558@gmail.com>
2025-07-13 04:19:39 +0200

    fix: update sitemap configuration and remove deprecated sitemap-0.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
+/** next-sitemap.config.js */
 module.exports = {
-  siteUrl: 'https://beauty-secrets-juliasun.vercel.app',
-  generateRobotsTxt: true,
-  // опционально: exclude: ['/secret-page']
-};
+  siteUrl: 'https://beauty-secrets-juliasun.vercel.app',  // ваш домен
+  generateIndexSitemap: false,   // НЕ делаем индексный sitemap-0.xml
+  sitemapSize: 7000,             // сколько URL в одном файле (у вас явно меньше)
+  outDir: './public',            // всё вывалится в папку public/
+}
