6f390089d81d4a1472fcee55987cbbdc3ebdfb94
mirejy <allo124558@gmail.com>
2025-07-13 12:39:34 +0200

    refactor: remove next-sitemap integration and update sitemap configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Disposition",
+            value: "",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
