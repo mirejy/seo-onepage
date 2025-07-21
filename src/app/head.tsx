abca6423614dc6c9683ee1e3a6487fdc2a82be3f
mirejy <allo124558@gmail.com>
2025-07-21 22:20:03 +0200

    Add Facebook Pixel tracking script to Head component for enhanced analytics

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,54 @@
-'use client';
-
-import SEO from './seo';
+// src/app/head.tsx
+import Script from "next/script";
+import SEO from "./seo";
 
 export default function Head() {
-  return <SEO />;
+  return (
+    <>
+      {/* Ваши SEO‑мета */}
+      <SEO />
+
+      {/* 1) Браузерный пиксель Meta */}
+      <Script id="fb-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s){
+            if(f.fbq)return;
+            n=f.fbq=function(){
+              n.callMethod?
+                n.callMethod.apply(n,arguments):
+                n.queue.push(arguments)
+            };
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+          }(
+            window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js'
+          );
+          fbq('init', '1076271397967446');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      {/* 2) Фолбэк для отключённого JS */}
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `
+            <img
+              height="1" width="1"
+              style="display:none"
+              src="https://www.facebook.com/tr?id=1076271397967446&ev=PageView&noscript=1"
+            />
+          `,
+        }}
+      />
+    </>
+  );
 }
